ce74d674551420f1a6443a20b323c40fa615894e
SuyogMahajan <mahajansuyog9322@gmail.com>
2021-12-18 21:52:23 +0530

    nfriends

---
+++ Nfriends.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int ways(int n){
+
+ int dp[1000] = {0};
+
+ dp[0] = dp[1] = 1;
+
+ for(int i = 2;i<=n;i++){
+  dp[i] = dp[i-1] + (i-1)*dp[i-2];
+ }
+
+ return dp[n];
+}
+
+int main(){
+    
+ 
+ 
+	int n;
+	cin >> n;
+
+	cout << ways(n) <<endl;
+
+}
